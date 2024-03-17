866a10bc9176af5c3677e9ebc1b7525bc3fe5e06
Priyanshu-ryu <pr.agarwal2005@gmail.com>
2024-03-17 12:37:52 +0530

    Added the script to count the lines in repote repo

---
+++ scripts/cloc-count-lines.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+if [ "$#" -eq 0 ]; then
+    echo "Kindly also pass the repository url for which you want to count the lines"
+    exit 1
+fi
+
+repo_url=$1
+cd ..
+
+temporary_directory=$(mktemp -d)
+git clone "$repo_url" "$temporary_directory"
+
+cd "$temporary_directory" || exit
+cloc .
+
+rm -rf "$temporary_directory"
